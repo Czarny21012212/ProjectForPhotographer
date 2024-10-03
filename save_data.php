d0622bb8eb0e13072f71f3a0141ea8c0ad67d8d7
Wiktor Stachula <136896196+Czarny21012212@users.noreply.github.com>
2024-10-03 22:06:24 +0200

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -3,27 +3,23 @@ header("Access-Control-Allow-Origin: *"); // Zezwala na żądania z dowolnego ź
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Zezwala na metody POST, GET i OPTIONS
 header("Access-Control-Allow-Headers: Content-Type"); // Zezwala na nagłówki Content-Type
 
-// Reszta kodu PHP
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "photographerjacek";
 
-// Połączenie z bazą danych
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Sprawdzenie połączenia
+$conn = new mysqli($servername, $username, $password, 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Odbiór danych z Reacta (z POST request)
+
 $name = $_POST['name'];
 $nazwisko = $_POST['nazwisko'];
 $email = $_POST['email'];
 $wiadomosc = $_POST['wiadomosc'];
 
-// Przygotowanie zapytania SQL
+
 $sql = "INSERT INTO wiadomości (Imie, Nazwisko, Email, Wiadomość) VALUES ('$name', '$nazwisko', '$email', '$wiadomosc')";
 
 if ($conn->query($sql) === TRUE) {
